44a6972fe029ccfce4068bbe5a8f8aa8a3c6bd78
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2025-11-25 21:47:29 -0300

    :recycle: refactor: remove lista de carros estática e realiza busca no backend

--- atividades/pratica-react-01/app-react-01/src/App.jsx
+++ atividades/pratica-react-01/app-react-01/src/App.jsx
@@ -4,47 +4,30 @@ import "./App.css";
 
 import Card from "./components/Card";
 import SearchBar from "./components/SearchBar";
-
-const listCars = [
-  {
-    id: 1,
-    brand: "Hyundai",
-    model: "HB20",
-    km: "65.000",
-    category: "Hatch",
-    year: "2018",
-    price: "55.000,00",
-  },
-  {
-    id: 2,
-    brand: "Toyota",
-    model: "Hilux",
-    km: "100.000",
-    category: "Picape",
-    year: "2019",
-    price: "155.000,00",
-  },
-  {
-    id: 3,
-    brand: "Volkswagen",
-    model: "T-Cross",
-    km: "20.000",
-    category: "SUV",
-    year: "2021",
-    price: "135.000,00",
-  },
-];
+import { useEffect } from "react";
 
 const App = () => {
-  const [cars, setCars] = useState(listCars);
+  const [cars, setCars] = useState(null);
 
-  const handleSearch = (query) => {
+  useEffect(() => {
+    async function fetchCars() {
+      const res = await fetch("http://localhost:3000/cars");
+      const data = await res.json();
+      setCars(data);
+    }
+
+    fetchCars();
+  }, []);
+
+  const handleSearch = async (query) => {
     if (query === '') {
-      setCars(listCars);
+      const res = await fetch("http://localhost:3000/cars");
+      const data = await res.json();
+      setCars(data);
       return;
-    } 
+    }
 
-    setCars(listCars.filter( ({ model }) => model.toLowerCase().includes(query.toLowerCase()) ));
+    setCars(cars.filter( ({ model }) => model.toLowerCase().includes(query.toLowerCase()) ));
   }
   
   return (
@@ -52,7 +35,7 @@ const App = () => {
       <h1>Anúncios Recentes</h1>
       <SearchBar handleSearch={handleSearch} />
       <div className='cards'>
-        {cars.map((car) => (
+        {cars && cars.map((car) => (
           <Card key={car.id} car={car} />
         ))}
       </div>
